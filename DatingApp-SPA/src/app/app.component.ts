d950756e19b818255905c6162294bc3e78ef2f4e
Ihssan Kitana <I.Kitana@eska.com>
2019-08-29 09:47:02 +0300

    End of section 6

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthService } from './_services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DatingApp-SPA';
+export class AppComponent implements OnInit {
+
+  jwtHelper = new JwtHelperService();
+
+  constructor(private service: AuthService) {}
+
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.service.decodedToken = this.jwtHelper.decodeToken(token);
+    }
+  }
+
 }
