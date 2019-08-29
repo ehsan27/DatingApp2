34875baa82c3320c9df7f126d9eee625d8cba3cc
Ihssan Kitana <I.Kitana@eska.com>
2019-08-29 15:20:09 +0300

    End of section 8

---
+++ DatingApp.API/Migrations/20190829091512_mod.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class mod : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Intrests",
+                table: "Users",
+                newName: "Interests");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Interests",
+                table: "Users",
+                newName: "Intrests");
+        }
+    }
+}
